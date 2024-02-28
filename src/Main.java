20ef306d8168da65ac1d5771505d6ef359191929
Daniel Kotsugai <d.kotsugai.sarmento@avanade.com>
2024-02-27 22:29:19 -0300

    Duas Linhas teste3

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Exercicio2");
-        System.out.println("TesteDuasLinhas");
+        System.out.println("Exercicio3\nTesteduasLinhas");
+
 
     }
 }
